df87acc924bbcfab8fdade5cd3c16fe6fd6ded90
dragonGR <alex@debtman.dev>
2026-09-04 13:51:27 +0300

    flatpak: Support offline builds with cargo vendored sources and GNOME 50 runtime

--- build-aux/cargo.sh
+++ build-aux/cargo.sh
@@ -9,10 +9,15 @@ BUILDTYPE="$4"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT/target"
 export CARGO_HOME="${CARGO_HOME:-$MESON_BUILD_ROOT/cargo-home}"
 
+CARGO_FLAGS=""
+if [ -f "$MESON_SOURCE_ROOT/cargo/config" ] || [ -f "$CARGO_HOME/config" ]; then
+    CARGO_FLAGS="--offline"
+fi
+
 if [ "$BUILDTYPE" = "release" ]; then
-    cargo build --manifest-path "$MESON_SOURCE_ROOT/Cargo.toml" --release
+    cargo build --manifest-path "$MESON_SOURCE_ROOT/Cargo.toml" --release $CARGO_FLAGS
     cp "$CARGO_TARGET_DIR/release/dropzone" "$OUTPUT"
 else
-    cargo build --manifest-path "$MESON_SOURCE_ROOT/Cargo.toml"
+    cargo build --manifest-path "$MESON_SOURCE_ROOT/Cargo.toml" $CARGO_FLAGS
     cp "$CARGO_TARGET_DIR/debug/dropzone" "$OUTPUT"
 fi
